25c13e28c5536ab502c7ce6a64d2f1f6023af1d5
giovanni facco <gfacco@gmail.com>
2022-06-16 13:41:31 -0700

    test: test contructor with errors for fallible_time_t

--- test/json_to_quaff_test.f90
+++ test/json_to_quaff_test.f90
@@ -1,5 +1,5 @@
 module json_to_quaff_test
-  use veggies, only: result_t, test_item_t, describe, it, succeed, fail
+  use veggies, only: assert_equals, result_t, test_item_t, describe, it, succeed, fail
   use json_to_quaff_m, only : fallible_time_t
   use quaff, only: operator(.unit.), SECONDS
   use rojff, only: fallible_json_value_t, parse_json_from_string
@@ -22,9 +22,9 @@ contains
         [ it( &
             "with no errors", &
             check_time_valid) &
-        ! , it( &
-        !     "with errors", &
-        !     check_time_time_with_errors) &
+        , it( &
+            "with errors", &
+            check_time_with_errors) &
         ]) &
     ])
   end function
@@ -47,6 +47,28 @@ contains
       result_ = fail(errors%to_string())
     else
       result_ = assert_equals(fallible_quaff_time%time(), time_r.unit.SECONDS)
+    end if
+
+  end function
+  function check_time_with_errors() result(result_)
+    type(result_t) :: result_
+    type(fallible_json_value_t) :: fallible_json_time
+    type(fallible_time_t) :: fallible_quaff_time
+    type(error_list_t) :: errors_quaff, errors_rojff
+    character(len=*), parameter :: not_a_json_c ='"1.0 s'
+    double precision, parameter :: time_r = 1.0d0
+
+
+    fallible_json_time = parse_json_from_string(not_a_json_c)
+
+    fallible_quaff_time = fallible_time_t(fallible_json_time)
+
+    if (fallible_quaff_time%failed()) then
+      errors_quaff = fallible_quaff_time%errors()
+      errors_rojff = fallible_json_time%errors
+      result_ = assert_equals(errors_quaff%to_string(), errors_rojff%to_string())
+    else
+      result_ = fail("fallible_quaff did not succesffuly retain errors from a failed fallible_json")
     end if
 
   end function

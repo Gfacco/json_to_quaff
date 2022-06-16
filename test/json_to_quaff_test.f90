f8a8fe34c3603178560bd528d0aa6f3d01901626
giovanni facco <gfacco@gmail.com>
2022-06-16 11:18:14 -0700

    feat: fallible_json to fallible_time

--- test/json_to_quaff_test.f90
+++ test/json_to_quaff_test.f90
@@ -1,9 +1,11 @@
 module json_to_quaff_test
   use veggies, only: result_t, test_item_t, describe, it, succeed, fail
-  ! use json_to_quaff_m, only : fallible_time_t
-  use quaff, only: fallible_time_t
+  use json_to_quaff_m, only : fallible_time_t
+  use quaff, only: operator(.unit.), SECONDS
   use rojff, only: fallible_json_value_t, parse_json_from_string
   use erloff, only: error_list_t
+  use quaff_asserts_m, only: assert_equals
+
 
 
   implicit none
@@ -33,14 +35,18 @@ contains
     type(fallible_time_t) :: fallible_quaff_time
     type(error_list_t) :: errors
     character(len=*), parameter :: time_c ='"1.0 s"'
+    double precision, parameter :: time_r = 1.0d0
+
 
     fallible_json_time = parse_json_from_string(time_c)
 
-    if (fallible_json_time%failed()) then
-      errors = fallible_json_time%errors
+    fallible_quaff_time = fallible_time_t(fallible_json_time)
+
+    if (fallible_quaff_time%failed()) then
+      errors = fallible_quaff_time%errors()
       result_ = fail(errors%to_string())
     else
-      result_ =succeed(time_c // ", was a valid JSON string")
+      result_ = assert_equals(fallible_quaff_time%time(), time_r.unit.SECONDS)
     end if
 
   end function
